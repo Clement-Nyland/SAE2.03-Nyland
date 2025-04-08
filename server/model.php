11475259362be9394ca816e985277f5c374c0c50
Nyland Clement <clement.nyland@etu.unilim.fr>
2025-04-08 10:33:57 +0200

    fix getAllMovies

--- server/model.php
+++ server/model.php
@@ -23,11 +23,14 @@ function getAllMovies(){
     $cnx = new PDO("mysql:host=".HOST.";dbname=".DBNAME, DBLOGIN, DBPWD);
     // Requête SQL pour récupérer le menu avec des paramètres
     $sql = "select id, name, image from Movie";
+ 
     // Lie le paramètre à la valeur
-    $stmt->$cnx->prepare($sql);
+    $stmt = $cnx->prepare($sql);
     // Exécute la requête SQL
+   
     $stmt->execute();
     // Récupère les résultats de la requête sous forme d'objets
     $res = $stmt->fetchAll(PDO::FETCH_OBJ);
+   
     return $res; // Retourne les résultats
 }
